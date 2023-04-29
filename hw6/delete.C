87b97765bc010d40d572697a8aef38234a1d8cb9
Yizheng(Jerry) Shi <85378870+yizhengshi@users.noreply.github.com>
2023-04-29 16:20:09 -0500

    initial commit to delete and insert

--- hw6/delete.C
+++ hw6/delete.C
@@ -16,10 +16,68 @@ const Status QU_Delete(const string & relation,
 		       const Datatype type, 
 		       const char *attrValue)
 {
-// part 6
-return OK;
+
+// This function will delete all tuples in relation satisfying 
+// the predicate specified by attrName, op, and the constant attrValue. 
+// type denotes the type of the attribute. 
+// You can locate all the qualifying tuples using a filtered HeapFileScan.
 
 
+
+	Status status;
+	HeapFileScan scan(relation, status); // used for locate all the qualifying tuples 
+	if (status != OK) return status;
+	RID rRID;
+	// int tupCnt = 0; // deleted record number	
+
+
+	// - - - case1: attrName is NULL, delete the entire records in relation - - -
+	if(attrName.length() == 0) {
+
+		//start scan, from begining, with filter = NULL, scan will return all the records in the file.
+		status = scan.startScan(0,0, STRING, NULL, EQ);
+		if (status != OK) return status;
+		while(scan.scanNext(rRID) == OK) {
+			status = scan.deleteRecord();
+			if (status != OK) return status;
+		}
+		return OK;
+	}
+
+	// - - - case2: with specific filter to delete - - - 
+	//get AttrDesc structure for the attribute
+	AttrDesc attrDesc;
+	status = attrCat->getInfo(relation, attrName, attrDesc);
+	if (status != OK) return status;
+	
+
+	// type denotes the type of the attribute. 
+	// A final note: the search value is always supplied as the character string attrValue. 
+	// You should convert it to the proper type based on the type of attr. 
+	// You can use the atoi()function to convert a char* to an integer and atof() to convert it to a float.  
+	// - - - type conversion - - -
+	int temp1;
+	float temp2;
+	const char* filter;
+	if (type == INTEGER)
+		temp1 = atoi(attrValue);
+		filter = (char*) &temp1;
+	if (type == FLOAT)
+		temp2 = atof(attrValue);
+		filter = (char*)&temp2;
+	if (type == STRING)
+		filter = attrValue;
+	// - - -
+
+	// - - -start scan, with specific filter value, attribute info in attrDesc, and operator. - - - go through entire pages & delete satisfied records
+	status = scan.startScan(attrDesc.attrOffset, attrDesc.attrLen, type, filter, op);
+	if (status != OK) return status;
+	while(scan.scanNext(rRID) == OK) {
+		status = scan.deleteRecord();
+		if(status != OK) return status;
+	}
+
+	return OK;
 
 }
 
--- hw6/insert.C
+++ hw6/insert.C
@@ -14,8 +14,84 @@ const Status QU_Insert(const string & relation,
 	const int attrCnt, 
 	const attrInfo attrList[])
 {
-// part 6
-return OK;
+
+
+// Insert a tuple with the given attribute values (in attrList) in relation. 
+// The value of the attributeis supplied in the attrValue member of the attrInfo structure.
+// Since the order of the attributes in attrList[] may not be the same as in the relation, 
+// 		you might have to rearrange them before insertion. 
+// If no value is specified for an attribute, you should reject the insertion as Minireldoes not implement NULLs
+
+
+// - - - reject insertion for NULLs attributes value
+	for (int i = 0; i <= attrCnt - 1; ++i) {
+		if (attrList[i].attrValue == NULL)
+			return ATTRTYPEMISMATCH;
+	}
+
+// - - - creating empty record, with length and unspecified data 
+// - - - get output record length from attrdesc structures
+   	Status status;
+	int attCnt; // actual attr size
+	AttrDesc *attrsArray; // actual attr name and type in correct order
+
+	status = attrCat->getRelInfo(relation, attCnt, attrsArray);
+	if (status != OK)return status;
+
+    int reclen = 0;
+    for (int i = 0; i < attCnt; i++)
+    {
+        reclen += attrsArray[i].attrLen;
+    }
+    
+    // open the result table
+    InsertFileScan resultRel(relation, status);
+    if (status != OK) return status;
+
+    char outputData[reclen];
+    Record outputRec;
+    outputRec.data = (void *) outputData;
+    outputRec.length = reclen;
+
+// - - --
+// - - - rearrange the attribute order - - - in Brute Force way O(n**2) - - Optimization here with O(n)- - -
+	int flag = 0;
+	int temp1 = 0;
+	float temp2 = 0;
+
+	// for each attr, check with name/type in actual relation attrList in the same position
+	for (int i = 0; i <= attrCnt - 1; i++) {
+		flag = 0;
+		for (int j = 0; j <= attrCnt - 1; j++) {
+			// 11111 matches attributes
+			if (strcmp(attrsArray[i].attrName, attrList[j].attrName) == 0 && attrsArray[i].attrType == attrList[j].attrType) {
+				flag = 1;
+				// moving actual data to the outputData, type conversion
+				switch(attrsArray[i].attrType){
+					case 0:
+						memcpy(outputData + attrsArray[i].attrOffset, (char *)attrList[j].attrValue, attrsArray[i].attrLen);
+						break;
+					case 1:
+						temp1 = atoi((char *)attrList[j].attrValue);
+						memcpy(outputData + attrsArray[i].attrOffset, (char *)&temp1, attrsArray[i].attrLen);
+						break;
+					case 2:
+						temp2 = atof((char *)attrList[j].attrValue);	
+						memcpy(outputData + attrsArray[i].attrOffset, (char *)&temp2, attrsArray[i].attrLen);
+						break;
+				}
+				return ATTRTYPEMISMATCH;
+			}
+		}
+		// 00000 no matches attributes
+		if (flag == 0) return ATTRTYPEMISMATCH;
+	}
+
+// - - - Insert
+	RID rid;
+	status = resultRel.insertRecord(outputRec, rid);
+	if (status != OK) return status;
+	return OK;
 
 }
 
